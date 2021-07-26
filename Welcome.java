9b136f58684a3453cd57d1eee09e96c6d2e11571
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-26 19:31:19 +0530

    Added:done

--- Welcome.java
+++ Welcome.java
@@ -4,9 +4,11 @@ public class Welcome {
 	public static void main(String args[])
 	{
 		System.out.println("welcome");
-		int a=10,b=20,c;
+		int a=10,b=20,c,d;
 		c=a+b;
 		System.out.println(c);
+		d=a*b;
+		System.out.println(d);
 		
 	}
 }
